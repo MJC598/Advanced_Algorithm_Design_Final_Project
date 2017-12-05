b5851a4008a2914b23f3cd2179ba0a55347f000c
Matthew Carroll <matthew.j.carroll@outlook.com>
2017-12-04 21:30:09 -0600

    made second maze and converted to integer

--- FinalProject/main.c
+++ FinalProject/main.c
@@ -7,10 +7,15 @@ int main(int argc, char* argv[]) {
   //   exit(INCORRRECT_NUM_ARGS);
   // }
   Maze maze;
+  IntMaze intMaze;
   init_vector(&maze);
+  init_intVector(&intMaze);
   maze = parse_getline(argv[1], maze);
+  intMaze = convertToInt(maze, intMaze);
   printMaze(maze);
+  printIntMaze(intMaze);
   free_vector(&maze);
+  free_intVector(&intMaze);
   return (EXIT_SUCCESS);
 }
 
@@ -22,22 +27,52 @@ Maze parse_getline(char* input, Maze maze) {
 	size_t nbytes = 0;
   int linelen=0;
 	while ((linelen=getline(&line, &nbytes, fp)) != -1) {
-		line[linelen-1] = '\0'; //removing the newline and adding the NULL character
-		//do whatever with line
+		line[linelen-1] = '\0';
     if(width < linelen){
       width = linelen;
     }
     int j = 0;
-
     for(j = 0; j < linelen; j++){
           insert_element_vector(&maze, line[j], counter, j);
     }
     counter++;
-
 	}
-
 	free(line);
   return(maze);
+}
+
+IntMaze convertToInt(Maze maze, IntMaze intMaze){
+  int i, j;
+  for(i = 0; i < vector_height(&maze); i++){
+    for(j = 0; j < vector_width(&maze); j++){
+      char temp = access_element_vector(&maze, i, j);
+      char space = ' ';
+      char hash = '#';
+      char rob1 = 'S';
+      char rob2 = 'F';
+      char end1 = 'E';
+      char end2 = 'L';
+      if(temp == space){
+        insert_element_intVector(&intMaze, 0, i, j);
+      }
+      else if(temp == hash){
+        insert_element_intVector(&intMaze, 1, i, j);
+      }
+      else if(temp == rob1){
+        insert_element_intVector(&intMaze, 2, i, j);
+      }
+      else if(temp == rob2){
+        insert_element_intVector(&intMaze, 3, i, j);
+      }
+      else if(temp == end1){
+        insert_element_intVector(&intMaze, 4, i, j);
+      }
+      else if(temp == end2){
+        insert_element_intVector(&intMaze, 5, i, j);
+      }
+    }
+  }
+  return(intMaze);
 }
 
 void printMaze(Maze maze){
@@ -47,6 +82,18 @@ void printMaze(Maze maze){
   for(i = 0; i < height; i++){
     for(j = 0; j < width; j++){
       printf("%c", access_element_vector(&maze, i, j));
+    }
+    printf("\n");
+  }
+}
+
+void printIntMaze(IntMaze intMaze){
+  int i, j;
+  int height = vector_intHeight(&intMaze);
+  int width = vector_intWidth(&intMaze);
+  for(i = 0; i < height; i++){
+    for(j = 0; j < width; j++){
+      printf("%d", access_element_intVector(&intMaze, i, j));
     }
     printf("\n");
   }
--- FinalProject/vector.h
+++ FinalProject/vector.h
@@ -22,13 +22,29 @@ typedef struct vector
   int widthcapacity;
 }Maze;
 
+typedef struct intVector{
+	int** data;
+	int height;
+	int width;
+	int heightcapacity;
+	int widthcapacity;
+}IntMaze;
+
 void init_vector(struct vector* v);
+void init_intVector(struct intVector* v);
 char access_element_vector(struct vector* v, size_t height, size_t width);
+char access_element_intVector(struct intVector* v, size_t height, size_t width);
 void insert_element_vector(struct vector* v, char element_to_insert, int height, int width);
+void insert_element_intVector(struct intVector* v, int element_to_insert, int height, int width);
 void free_vector(struct vector* v);
+void free_intVector(struct intVector* v);
 int vector_height(struct vector* v);
+int vector_intHeight(struct intVector* v);
 int vector_width(struct vector* v);
+int vector_intWidth(struct intVector* v);
 Maze parse_getline(char* input, Maze maze);
 void printMaze(Maze maze);
+void printIntMaze(IntMaze maze);
+IntMaze convertToInt(Maze maze, IntMaze intMaze);
 
 #endif
